c88d6345c41f66e3d64eaffb4732b2c84a7ec2b2
James Ortiz <jameswortiz728@gmail.com>
2023-05-22 20:41:38 -0400

    Styled new Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
          <footer className="footer">
             <div className="content-container">
-                <h1>Contact me at james@jwortiz.io for any inquiries</h1>
+                <Link to="https://www.jwortiz.dev/"><h1>Return to JWOrtiz.dev</h1></Link>
             </div>
         </footer>
     )
